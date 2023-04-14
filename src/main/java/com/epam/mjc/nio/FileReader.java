5d6494c36fdc529c9f49dc30dfedff13163a5320
ArimaSeko <120338228+ArimaSeko@users.noreply.github.com>
2023-04-15 00:35:58 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -7,47 +7,44 @@ import java.nio.channels.FileChannel;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        String name;
-        int age=0;
-        String email;
-        long phone=0;
-        String data=null;
+        String name = null;
+        int age = 0;
+        String email = null;
+        long phone = 0;
+        String data;
+        String strAge;
+        String strPhone;
         try {
             data = String.valueOf(FileReader.readFromFileUsingFileChannel(file));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        String strAge;
-        String strPhone ;
-        try{strAge = cut(data, "Age: ");
+            strAge = cut(data, "Age: ");
             email = cut(data, "Email: ");
             strPhone = cut(data, "Phone: ");
             name = cut(data, "Name: ");
             age = Integer.parseInt(strAge.trim());
             phone = Long.parseLong(strPhone.trim());
-            return new Profile(name,age,email,phone);
-        }catch (NullPointerException|NumberFormatException e){e.printStackTrace();
-            return null;}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new Profile(name, age, email, phone);
     }
 
     public static String cut(String text, String find) {
-         if(text !=null){
-        String answer = "";
-        int start = text.indexOf(find);
-        text = text.substring(start);
-        start = text.indexOf(find);
-        int end = text.indexOf("\n");
-        if (start > -1)answer = text.substring(start + find.length(), end);
-        return answer;}else return null;
+        if (text != null) {
+            String answer = "";
+            int start = text.indexOf(find);
+            text = text.substring(start);
+            start = text.indexOf(find);
+            int end = text.indexOf("\n");
+            if (start > -1) answer = text.substring(start + find.length(), end);
+            return answer;
+        } else return null;
     }
+
     public static StringBuilder readFromFileUsingFileChannel(File rfile) throws IOException {
 
         StringBuilder content = null;
         ByteBuffer buffer;
-        RandomAccessFile file=null;
-        try {
-            file = new RandomAccessFile(rfile, "r");
+        try (RandomAccessFile file = new RandomAccessFile(rfile, "r")) {
             FileChannel channel = file.getChannel();
             content = new StringBuilder();
             buffer = ByteBuffer.allocate(256);
@@ -59,18 +56,13 @@ public class FileReader {
                 }
                 buffer.clear();
                 bytesRead = channel.read(buffer);
-
             }
-            return content;
-        }
-        catch (IOException e) {
+        } catch (NullPointerException e) {
             e.printStackTrace();
-            return null;
-        }finally {
-            file.close();
         }
-        }
+        return content;
     }
+}
 
 
 
