e45f10397583f059be36a9bc5c6174270edf0df0
ArimaSeko <120338228+ArimaSeko@users.noreply.github.com>
2023-04-15 00:08:22 +0600

    Update FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -11,37 +11,42 @@ public class FileReader {
         int age=0;
         String email;
         long phone=0;
-        String data = "";
+        String data=null;
         try {
             data = String.valueOf(FileReader.readFromFileUsingFileChannel(file));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        String strAge = cut(data, "Age: ");
+        String strAge;
         email = cut(data, "Email: ");
-        String strPhone = cut(data, "Phone: ");
+        String strPhone ;
         name = cut(data, "Name: ");
-        try{
+        try{strAge = cut(data, "Age: ");
+            email = cut(data, "Email: ");
+            strPhone = cut(data, "Phone: ");
+            name = cut(data, "Name: ");
             age = Integer.parseInt(strAge.trim());
-            phone = Long.parseLong(strPhone.trim());}
-        catch (NumberFormatException e){e.printStackTrace();}
+            phone = Long.parseLong(strPhone.trim());
+        }catch (NullPointerException|NumberFormatException e){e.printStackTrace();}
         return new Profile(name,age,email,phone);
     }
 
     public static String cut(String text, String find) {
-
+         if(text !=null){
         String answer = "";
         int start = text.indexOf(find);
         text = text.substring(start);
         start = text.indexOf(find);
         int end = text.indexOf("\n");
         if (start > -1)answer = text.substring(start + find.length(), end);
-        return answer;
+        return answer;}else return null;
     }
-    public static StringBuilder readFromFileUsingFileChannel(File rfile) {
-        RandomAccessFile file = null;
-        StringBuilder content=null;
-        ByteBuffer buffer = null;
+    public static StringBuilder readFromFileUsingFileChannel(File rfile)  {
+
+        RandomAccessFile file ;
+        StringBuilder content = null;
+        ByteBuffer buffer;
+
         try {
             file = new RandomAccessFile(rfile, "r");
             FileChannel channel = file.getChannel();
@@ -56,16 +61,9 @@ public class FileReader {
                 buffer.clear();
                 bytesRead = channel.read(buffer);
             }
-        } catch (FileNotFoundException e) {
+            file.close();
+        }catch (IOException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            try {
-                file.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
         return content;
     }
